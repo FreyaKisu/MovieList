4275a8c3df8086da364e56627b53299e88024129
FreyaNoir <freyanoir@gmail.com>
2018-11-28 12:06:45 +0200

    pom.xml plugin for debug; login page format

--- src/main/java/fi/project/movielist/WebSecurityConfig.java
+++ src/main/java/fi/project/movielist/WebSecurityConfig.java
@@ -29,6 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		
 		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
 	}
 }
